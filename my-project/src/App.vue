26961c7fe8f657441a3a5b16bf9969bc1df7eed6
Software Engineer 16 <Software.Engineer16@bafokeng.local>
2024-08-06 11:15:44 +0200

    Added documentation for App.vue

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -9,10 +9,25 @@
 </template>
 
 <script>
+/**
+ * @fileoverview This is the main layout component for the application.
+ * It includes a Navbar and a router-view for rendering matched components based on the current route.
+ */
+
 import Navbar from './components/Navbar.vue';
 
+/**
+ * Main layout component for the application.
+ * It includes the Navbar component and the router-view.
+ *
+ * @component
+ */
 export default {
   components: {
+    /** 
+     * The Navbar component used for navigation throughout the app.
+     * @type {import('./components/Navbar.vue').default}
+     */
     Navbar
   }
 };
