04b1bb2360cd1570d3609ecc432b76c61547f119
Software Engineer 16 <Software.Engineer16@bafokeng.local>
2024-08-06 15:37:00 +0200

    debugged my code by using  the correct version of vue

--- my-project/src/components/NavBar.vue
+++ my-project/src/components/NavBar.vue
@@ -1,41 +1,50 @@
 <template>
-  <header class="sticky top-0 z-50 bg-gray-500 border-gray-200">
-    <nav class="bg-gray-500 border-gray-200">
-      <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <!-- Brand name -->
-        <a class="flex items-center space-x-3 rtl:space-x-reverse" href="/">
-          <img src="../assets/online-shop.png" class="h-8" alt="SwiftCart">
-          <span class="text-2xl font-semibold text-white">SwiftCart</span>
-        </a>
-
-        <!-- Menu toggle button for smaller screens -->
-        <button @click="toggleMenu" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-white rounded-lg md:hidden hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-dropdown" aria-expanded="false">
-          <span class="sr-only">Open main menu</span>
-          <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"/>
+  <nav class="fixed w-full bg-gray-400 p-4 text-gray-50 shadow-lg">
+    <div class="container mx-auto flex justify-between items-center">
+      <!-- Brand name -->
+      <a class="text-lg font-bold flex items-center" href="#">
+        <img class="w-8 mr-2" src="../assets/online-shop.png" alt="SwiftCart">
+        SwiftCart
+      </a>
+      
+      <!-- Navigation links for larger screens -->
+      <div class="hidden md:flex">
+        <a href="/" class="mr-4 hover:underline" >Wishlist</a>
+        <a href="./" class="mr-4 hover:underline">Cart</a>
+        <a href="./" class="mr-4 hover:underline">
+          <svg width="32px" height="32px" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M7.5 18C8.32843 18 9 18.6716 9 19.5C9 20.3284 8.32843 21 7.5 21C6.67157 21 6 20.3284 6 19.5C6 18.6716 6.67157 18 7.5 18Z" stroke="#1C274C" stroke-width="1.5"/>
+            <path d="M16.5 18.0001C17.3284 18.0001 18 18.6716 18 19.5001C18 20.3285 17.3284 21.0001 16.5 21.0001C15.6716 21.0001 15 20.3285 15 19.5001C15 18.6716 15.6716 18.0001 16.5 18.0001Z" stroke="#1C274C" stroke-width="1.5"/>
+            <path d="M11 10.8L12.1429 12L15 9" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M2 3L2.26121 3.09184C3.5628 3.54945 4.2136 3.77826 4.58584 4.32298C4.95808 4.86771 4.95808 5.59126 4.95808 7.03836V9.76C4.95808 12.7016 5.02132 13.6723 5.88772 14.5862C6.75412 15.5 8.14857 15.5 10.9375 15.5H12M16.2404 15.5C17.8014 15.5 18.5819 15.5 19.1336 15.0504C19.6853 14.6008 19.8429 13.8364 20.158 12.3075L20.6578 9.88275C21.0049 8.14369 21.1784 7.27417 20.7345 6.69708C20.2906 6.12 18.7738 6.12 17.0888 6.12H11.0235M4.95808 6.12H7" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"/>
           </svg>
-        </button>
-
-        <!-- Navigation links and dropdown menu -->
-        <div :class="{'hidden': !open, 'block': open}" class="md:flex md:w-auto" id="navbar-dropdown">
-          <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-500 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
-            <li>
-              <a href="/" class="block py-2 px-3 text-white rounded hover:bg-gray-700 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Wishlist</a>
-            </li>
-            <li class="relative">
-              <a href="./" class="block py-2 px-3 text-white rounded hover:bg-gray-700 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">
-                Cart
-                <span v-if="cartItemCount > 0" class="absolute top-0 right-0 -translate-x-1/2 translate-y-1/2 flex h-2 w-2 items-center justify-center rounded-full bg-red-500 text-xs text-white">{{ cartItemCount }}</span>
-              </a>
-            </li>
-            <li>
-              <a href="./" class="block py-2 px-3 text-white rounded hover:bg-gray-700 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Login</a>
-            </li>
-          </ul>
-        </div>
+        </a>
+        <a href="./" class="hover:underline">Login</a>
       </div>
-    </nav>
-  </header>
+      
+      <!-- Menu toggle button for smaller screens -->
+      <button @click="toggleMenu" class="md:hidden text-gray-100">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-6 h-6">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"/>
+        </svg>
+      </button>
+    </div>
+    
+    <!-- Mobile menu for smaller screens -->
+    <div v-if="open" class="md:hidden mt-4 bg-gray-300">
+      <a href="/" class="block p-2 hover:bg-gray-200">Wishlist</a>
+      <a href="./" class="block p-2 hover:bg-gray-200">Cart</a>
+      <a href="./" class="block p-2 hover:bg-gray-200">
+        <svg width="32px" height="32px" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M7.5 18C8.32843 18 9 18.6716 9 19.5C9 20.3284 8.32843 21 7.5 21C6.67157 21 6 20.3284 6 19.5C6 18.6716 6.67157 18 7.5 18Z" stroke="#1C274C" stroke-width="1.5"/>
+          <path d="M16.5 18.0001C17.3284 18.0001 18 18.6716 18 19.5001C18 20.3285 17.3284 21.0001 16.5 21.0001C15.6716 21.0001 15 20.3285 15 19.5001C15 18.6716 15.6716 18.0001 16.5 18.0001Z" stroke="#1C274C" stroke-width="1.5"/>
+          <path d="M11 10.8L12.1429 12L15 9" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+          <path d="M2 3L2.26121 3.09184C3.5628 3.54945 4.2136 3.77826 4.58584 4.32298C4.95808 4.86771 4.95808 5.59126 4.95808 7.03836V9.76C4.95808 12.7016 5.02132 13.6723 5.88772 14.5862C6.75412 15.5 8.14857 15.5 10.9375 15.5H12M16.2404 15.5C17.8014 15.5 18.5819 15.5 19.1336 15.0504C19.6853 14.6008 19.8429 13.8364 20.158 12.3075L20.6578 9.88275C21.0049 8.14369 21.1784 7.27417 20.7345 6.69708C20.2906 6.12 18.7738 6.12 17.0888 6.12H11.0235M4.95808 6.12H7" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"/>
+        </svg>
+      </a>
+      <a href="./" class="block p-2 hover:bg-gray-400">Login</a>
+    </div>
+  </nav>
 </template>
 
 <script>
@@ -46,7 +55,7 @@ export default {
 
   setup() {
     const open = ref(false);
-    const cartItemCount = ref(2); 
+
     const toggleMenu = () => {
       open.value = !open.value;
     };
@@ -54,12 +63,7 @@ export default {
     return {
       open,
       toggleMenu,
-      cartItemCount,
     };
   },
 };
 </script>
-
-<style scoped>
-
-</style>
