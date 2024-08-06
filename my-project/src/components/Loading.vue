fa799592d96d46b6ca088d0bba91a7402b48f8ab
Software Engineer 16 <Software.Engineer16@bafokeng.local>
2024-08-06 10:48:59 +0200

    Create Loading.vue

---
+++ my-project/src/components/Loading.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="loading text-center text-2xl justify-center items-center">
+      Loading...
+    </div>
+  </template>
+  
+  <script>
+  export default {
+     name: 'LoadingComponent',
+  };
+  </script>
+  
+  
