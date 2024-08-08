ecf3e5184b8228274625805a21dc70ad9f803f70
Software Engineer 16 <Software.Engineer16@bafokeng.local>
2024-08-08 08:58:08 +0200

    Added JSDocs.

--- my-project/src/main.js
+++ my-project/src/main.js
@@ -2,4 +2,11 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 
-createApp(App).use(router).mount('#app');
+/**
+ * Initializes and mounts the Vue application.
+ * @function
+ * @returns {void}
+ */
+createApp(App)
+  .use(router)
+  .mount('#app');
