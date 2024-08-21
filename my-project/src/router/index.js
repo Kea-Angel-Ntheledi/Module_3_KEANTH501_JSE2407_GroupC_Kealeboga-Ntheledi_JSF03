bc374374ba55386d2f0aa2f9fbe36bd5498b22f3
Kea-Angel-Ntheledi <kealebogaangelntheledi@gmail.com>
2024-08-21 14:10:43 +0200

    Added new routes for Login, Wishlist, and Cart, and implemented global navigation guard for authentication

--- my-project/src/router/index.js
+++ my-project/src/router/index.js
@@ -1,18 +1,34 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/Home.vue'; // Path to Home.vue
+import Home from '../views/Home.vue';
+import Login from '../views/Login.vue';
 import ProductDetail from '../views/ProductDetails.vue';
- // Path to ProductDetail.vue
+import Wishlist from '../views/Wishlist.vue';
+import Cart from '../views/Cart.vue'
 
 const routes = [
-  { path: '/', component: Home },
-  { path: '/product/:id', component: ProductDetail, props: true }
+  { path: '/', name:'home', component: Home },
+  { path: '/login', name:'login', component: Login },
+  { path: '/product/:id', name:'product', component: ProductDetail, props: true, meta: { requiresAuth: true } },
+  { path: '/wishlist', name:'wishlist', component: Wishlist },
+  { path: '/cart', name:'cart', component: Cart }
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory('/'),  // Updated to use a static base URL
   routes
 });
 
-export default router;
+// Global Navigation Guard
+router.beforeEach((to, from, next) => {
+  const loggedIn = localStorage.getItem('jwt');
 
+  if (to.matched.some(record => record.meta.requiresAuth) && !loggedIn) {
+    localStorage.setItem('redirectPath', to.fullPath);
+    next('/login');
+  } else {
+    next();
+  }
+});
+
+export default router;
 
