ee3dea98b09cb19cff5c69a5398e22c6abaf2705
Kea-Angel-Ntheledi <kealebogaangelntheledi@gmail.com>
2024-08-21 15:05:26 +0200

    Updated package.json, refactored App.vue, Navbar.vue, ProductGrid.vue, Cart.vue, ProductDetails.vue, and Wishlist.vue with changes to styles, components, and script logic.

--- my-project/src/main.js
+++ my-project/src/main.js
@@ -1,12 +1,13 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import router from './router';
+import routing from './router';
+import { createPinia } from 'pinia'; 
+import 'sweetalert2/dist/sweetalert2.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
-/**
- * Initializes and mounts the Vue application.
- * @function
- * @returns {void}
- */
-createApp(App)
-  .use(router)
-  .mount('#app');
+const pinia = createPinia();
+const app = createApp(App);
+
+app.use(pinia);
+app.use(routing);
+app.mount('#app');
